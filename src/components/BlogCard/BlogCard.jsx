64f8b37d415dcbe0ad3f1ac037ea6039172ee10c
Sofi Dobriak <sofi.dobriak@gmail.com>
2025-03-21 19:16:03 +0200

    task #1 is done

--- src/components/BlogCard/BlogCard.jsx
+++ src/components/BlogCard/BlogCard.jsx
@@ -1,4 +1,42 @@
-const BlogCard = () => {
-  return <h2>BlogCard</h2>;
+import styles from './BlogCard.module.css';
+import { formatDistanceToNow } from 'date-fns';
+
+const BlogCard = ({
+  poster,
+  tag,
+  title,
+  description,
+  name,
+  avatar,
+  postedAt,
+}) => {
+  const timeAgo = formatDistanceToNow(postedAt, { addSuffix: true });
+
+  return (
+    <>
+      <h2>BlogCard</h2>
+
+      <div className={styles.card}>
+        <div className={styles.cardHeader}>
+          <img className={styles.cardPoster} src={poster} alt="card__image" />
+        </div>
+        <div className={styles.cardBody}>
+          <span className={styles.tag}>{tag}</span>
+          <h2 className={styles.cardTitle}>{title}</h2>
+          <p className={styles.cardText}>{description}</p>
+        </div>
+        <div className={styles.cardFooter}>
+          <div className={styles.userBox}>
+            <img className={styles.avatar} src={avatar} alt="Jane Doe" />
+            <div>
+              <h3 className={styles.userName}>{name}</h3>
+              <small className={styles.date}>{timeAgo}</small>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 };
+
 export default BlogCard;
